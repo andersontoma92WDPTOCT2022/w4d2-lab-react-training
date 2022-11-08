583f5a71839cd3ae47536f744adebe01bd338498
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-11-08 17:59:40 -0300

    Done! até iteração 7

---
+++ src/components/DriverCard.js
@@ -0,0 +1,18 @@
+import Rating from './Rating';
+
+function DriverCard({ name, rating, img, car }) {
+  return (
+    <div className="drivecar">
+      <img src={img} alt="imagem" className="foto" />
+      <article className="driverData">
+        <h4 className="driverData">{name}</h4>
+        <Rating className="driverData">{rating}</Rating>
+        <p className="driverData">
+          {car.model} - {car.licensePlate}
+        </p>
+      </article>
+    </div>
+  );
+}
+
+export default DriverCard;
