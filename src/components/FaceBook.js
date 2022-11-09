c694f20a28444914461aced71bd0cc0a640a2500
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-11-08 21:07:12 -0300

    Done 1 a 7, +8, +13 com delete

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import profiles from '../data/berlin.json';
 
 function FaceBook() {
+  const [profileList, setProfileList] = useState(profiles);
+  const deletar = (img) => {
+    console.log('clicou', img);
+    const filteredProfile = profileList.filter((profile) => {
+      console.log(profile.img, profile.img !== img, '<---');
+      return profile.img !== img;
+    });
+    setProfileList(filteredProfile);
+  };
   return (
     <div>
-      {profiles.map(({ firstName, country, lastName, img, isStudent }) => {
+      {profileList.map(({ firstName, country, lastName, img, isStudent }) => {
         return (
-          <div className="idCard">
+          <div className="idCard" key={img}>
             <img src={img} alt="foto" className="foto" />
             <article>
               <div>firstName {firstName}</div>
               <div>lastName {lastName}</div>
               <div>country {country}</div>
+              <button onClick={() => deletar(img)}> delete </button>
             </article>
           </div>
         );
